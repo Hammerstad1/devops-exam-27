3c72fd4e292bebaf54f2e61d710d148775e43e0e
Lucas <lucashammerstad@gmail.com>
2025-11-19 10:59:21 +0100

    Format terraform filles

--- infra-s3/variables.tf
+++ infra-s3/variables.tf
@@ -3,29 +3,29 @@ variable "aws_region" {
 }
 variable "bucket_name" {
   description = "kandidat-27-data"
-  type = string
+  type        = string
 }
 
 variable "temp_prefix" {
   description = "Prefix for temporary files that lifecycle rules should target"
-  type = string
-  default = "midlertidig/"
+  type        = string
+  default     = "midlertidig/"
 }
 
 variable "transition_days" {
   description = "Number of days before temporary file sare moved to cheaper storage (Glacier)"
-  type = number
-  default = 30
+  type        = number
+  default     = 30
 }
 
 variable "expiration_days" {
   description = "Number of days before temporary files are deleted"
-  type = number
-  default = 90
+  type        = number
+  default     = 90
 }
 
 variable "transition_storage_class" {
   description = "GLACIER"
-  type = string
-  default = "GLACIER"
+  type        = string
+  default     = "GLACIER"
 }
